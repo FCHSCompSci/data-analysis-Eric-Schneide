8755072695d1d4b10c8e31e9df4c93226bdf4ff2
ESSCHN01 <eric.schneider@stu.jefferson.kyschools.us>
2018-12-18 07:50:38 -0500

    commit message

--- SAT scores.py
+++ SAT scores.py
@@ -3,31 +3,23 @@ import matplotlib.pyplot as plt
 
 filename='dcps_SATScores.csv'
 
-
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
-
-    for index,columb_header in enumerate(header_row):
-        print(index, columb_header)
-
     readingscore=[]
     writingscore=[]
     mathscore=[]
-    overallscore=[]
     for row in reader:
         readingscore.append(int(row[3]))
         writingscore.append(int(row[4]))
         mathscore.append(int(row[5]))
-        overallscore.append(int(row[6]))
     avg_r_score=(sum(readingscore)/len(readingscore))
     avg_w_score=(sum(writingscore)/len(writingscore))
     avg_m_score=(sum(mathscore)/len(mathscore))
-    avg_o_score=(sum(overallscore)/len(overallscore))
+    avg_o_score=((avg_m_score+avg_w_score+avg_r_score)/3)
 
-x_values=['Reading Score','Writing Score','Math Score']
-y_values=[avg_r_score,avg_w_score,avg_m_score]
+x_values=['Reading Score','Writing Score','Math Score','Overall Score']
+y_values=[avg_r_score,avg_w_score,avg_m_score,avg_o_score]
 
 plt.xlabel('SAT Catagory')
 plt.ylabel('Average Score')
